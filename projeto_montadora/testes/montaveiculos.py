adfe9cababb7a316ebab02538413b568b06a039b
lucasscantu <lucascantu05@gmail.com>
2025-11-26 00:10:56 -0300

    alterado main para realizar todas as tarefas

--- projeto_montadora/testes/montaveiculos.py
+++ projeto_montadora/testes/montaveiculos.py
@@ -1,3 +1,5 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from models.montadora import Montadora
 from models.observer import VendasObserver, QualidadeObserver, EstoqueObserver
 
